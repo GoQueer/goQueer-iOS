4cb626dae279e1da3b8ec0f85c34cf62f7a506e2
Lion User <User@Lions-Mac.local>
2017-05-20 02:51:56 +0100

    some more work yeyyy

--- AKSwiftSlideMenu/HomeVC.swift
+++ AKSwiftSlideMenu/HomeVC.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
-
+import MapKit
 class HomeVC: BaseViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         addSlideMenuButton()
-        // Do any additional setup after loading the view.
+        let initialLocation = CLLocation(latitude: 53.5563, longitude: -113.5186)
+        centerMapOnLocation(location: initialLocation)
+        
+                // Do any additional setup after loading the view.
     }
+    
+    let regionRadius: CLLocationDistance = 5000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+	
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
