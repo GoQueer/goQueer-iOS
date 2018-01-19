ae21215c38d947acf8db095d4585e2129b23bf63
bamdad <bamdad@bamdads-mac.local>
2018-01-18 20:24:54 -0800

    added qprofile

---
+++ QProfile.swift
@@ -0,0 +1,20 @@
+import Foundation
+import UIKit
+import MapKit
+import CoreLocation
+
+import Foundation
+class QProfile {
+    var names:String!
+    var description:String!
+    var show:String!
+    var lat:String!
+    var lan:String!
+    var zoom:String!
+    var bearing: String!
+    var tilt: String!
+    var visibleToPlayer: String!
+    
+}
+
+
